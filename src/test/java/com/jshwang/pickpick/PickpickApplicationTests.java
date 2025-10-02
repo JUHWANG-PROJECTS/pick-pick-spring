daa9261cb1eb8d4be3d373489a975223a2628a99
Hwang Junsu <junsu0325@naver.com>
2025-10-03 03:48:54 +0900

    Spring Initial Setup

---
+++ src/test/java/com/jshwang/pickpick/PickpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshwang.pickpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
